f20d66d66149c3ac360fb48fb927fd0914687ea2
Diego-Lucar <lucardiego@gmail.com>
2026-07-08 01:22:06 -0500

    add login

---
+++ inventario/src/main/java/com/proyecto/sistema/inventario/service/EmpleadoService.java
@@ -0,0 +1,44 @@
+package com.proyecto.sistema.inventario.service;
+
+import java.util.List;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.proyecto.sistema.inventario.entities.Empleado;
+import com.proyecto.sistema.inventario.entities.Rol;
+import com.proyecto.sistema.inventario.repository.EmpleadoRepository;
+
+
+@Service
+public class EmpleadoService {
+    private final EmpleadoRepository empleadoRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public EmpleadoService(EmpleadoRepository empleadoRepository, PasswordEncoder passwordEncoder) {
+        this.empleadoRepository = empleadoRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public List<Empleado> listarEmpleados(){
+        return empleadoRepository.findAll();
+    }
+
+    public void guardarEmpleado (Empleado empleado){
+        String password = empleado.getPassword();
+        if (password == null || password.isEmpty() || !password.startsWith("$2")) {
+            empleado.setPassword(passwordEncoder.encode(password));
+        }
+        empleadoRepository.save(empleado);
+    }
+
+    public void eliminarEmpleado (Long id){
+        empleadoRepository.deleteById(id);
+    }
+
+    public Empleado asignarRol(Long id, Rol rol){
+        Empleado emp = empleadoRepository.findById(id).orElseThrow();
+        emp.setRol(rol);
+        return empleadoRepository.save(emp);
+    }
+}
